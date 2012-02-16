f50bd7d969f5e22c4a0eafa84dff5a5d11bb8438
kurko <chavedomundo@gmail.com>
2012-02-16 02:01:04 -0200

    Tried making raphaeljs have more than one graph per page.

--- app/assets/javascripts/admin/raphael_main.js
+++ app/assets/javascripts/admin/raphael_main.js
@@ -2,9 +2,9 @@ $(function(){
   
   var data = {
     items: [
-      {label: 'Tênis', data: 12015},
-      {label: 'Camisetas', data: 23834},
-      {label: 'Calças', data: 24689}
+      {label: 'One', data: 12015},
+      {label: 'Two', data: 23834},
+      {label: 'Three', data: 24689}
     ]
   };
 
@@ -19,24 +19,33 @@ $(function(){
 
   var r = Raphael("pie_chart");
   var pie = r.piechart(60, 60, 50, values, {legend: labels, legendpos: "east"});
-  pie.attr({opacity: 0.7});
-  pie.hover(function () {
-    this.sector.stop();
-    this.sector.animate({opacity: 1}, 500);
-    this.sector.animate({ transform: 's1.05 1.05 ' + this.cx + ' ' + this.cy }, 500, "bounce");
-    if (this.label) {
-      this.label[0].stop();
-      this.label[0].attr({ r: 6 }, 800, "bounce");
-      this.label[1].attr({ "font-weight": 800 });
-    }
-  }, function () {
-    this.sector.animate({ transform: 's1 1 ' + this.cx + ' ' + this.cy }, 500, "bounce");
-    this.sector.attr({opacity: 0.7});
 
-    if (this.label) {
-        this.label[0].attr({ r: 5 }, 500, "bounce");
-        this.label[1].attr({ "font-weight": 400 });
-    }
-  });
+  var r_two = Raphael("pie_chart_two");
+  var pie_two = r_two.piechart(60, 60, 50, values, {legend: labels, legendpos: "east"});
+
+  var r_three = Raphael("pie_chart_three");
+  var pie_three = r_three.piechart(60, 60, 50, values, {legend: labels, legendpos: "east"});
+
+  // pie.attr({opacity: 0.7});
+  // pie.hover(function () {
+  //   this.sector.stop();
+  //   this.sector.animate({opacity: 1}, 500);
+  //   this.sector.animate({ transform: 's1.05 1.05 ' + this.cx + ' ' + this.cy }, 500, "bounce");
+  //   if (this.label) {
+  //     this.label[0].stop();
+  //     this.label[0].attr({ r: 6 }, 800, "bounce");
+  //     this.label[1].attr({ "font-weight": 800 });
+  //   }
+  // }, function () {
+  //   this.sector.animate({ transform: 's1 1 ' + this.cx + ' ' + this.cy }, 500, "bounce");
+  //   this.sector.attr({opacity: 0.7});
+
+  //   if (this.label) {
+  //       this.label[0].attr({ r: 5 }, 500, "bounce");
+  //       this.label[1].attr({ "font-weight": 400 });
+  //   }
+  // });
+
+
 
 })
