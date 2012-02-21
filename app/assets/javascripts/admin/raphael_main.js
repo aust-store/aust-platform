8b12eddf3c7ec17b49a07fb377970b19bf885257
kurko <chavedomundo@gmail.com>
2012-02-21 20:46:12 -0200

    Raphael graphics

--- app/assets/javascripts/admin/raphael_main.js
+++ app/assets/javascripts/admin/raphael_main.js
@@ -26,25 +26,25 @@ $(function(){
   var r_three = Raphael("pie_chart_three");
   var pie_three = r_three.piechart(60, 60, 50, values, {legend: labels, legendpos: "east"});
 
-  // pie.attr({opacity: 0.7});
-  // pie.hover(function () {
-  //   this.sector.stop();
-  //   this.sector.animate({opacity: 1}, 500);
-  //   this.sector.animate({ transform: 's1.05 1.05 ' + this.cx + ' ' + this.cy }, 500, "bounce");
-  //   if (this.label) {
-  //     this.label[0].stop();
-  //     this.label[0].attr({ r: 6 }, 800, "bounce");
-  //     this.label[1].attr({ "font-weight": 800 });
-  //   }
-  // }, function () {
-  //   this.sector.animate({ transform: 's1 1 ' + this.cx + ' ' + this.cy }, 500, "bounce");
-  //   this.sector.attr({opacity: 0.7});
+  pie.attr({opacity: 0.7});
+  pie.hover(function () {
+    this.sector.stop();
+    this.sector.animate({opacity: 1}, 500);
+    this.sector.animate({ transform: 's1.05 1.05 ' + this.cx + ' ' + this.cy }, 500, "bounce");
+    if (this.label) {
+      this.label[0].stop();
+      this.label[0].attr({ r: 6 }, 800, "bounce");
+      this.label[1].attr({ "font-weight": 800 });
+    }
+  }, function () {
+    this.sector.animate({ transform: 's1 1 ' + this.cx + ' ' + this.cy }, 500, "bounce");
+    this.sector.attr({opacity: 0.7});
 
-  //   if (this.label) {
-  //       this.label[0].attr({ r: 5 }, 500, "bounce");
-  //       this.label[1].attr({ "font-weight": 400 });
-  //   }
-  // });
+    if (this.label) {
+        this.label[0].attr({ r: 5 }, 500, "bounce");
+        this.label[1].attr({ "font-weight": 400 });
+    }
+  });
 
 
 
