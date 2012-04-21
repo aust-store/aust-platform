007833f46b42808f644c7a015c32b37d851a6054
Rafael <rafael.leite.oliveira@gmail.com>
2012-04-21 16:21:25 -0300

    shema ok

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,23 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120415012630) do
+ActiveRecord::Schema.define(:version => 20120417020928) do
+
+  create_table "account_receivables", :force => true do |t|
+    t.integer  "company_id"
+    t.integer  "admin_user_id"
+    t.integer  "customer_id"
+    t.decimal  "value"
+    t.text     "description"
+    t.date     "due_to"
+    t.boolean  "paid"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
+  add_index "account_receivables", ["admin_user_id"], :name => "index_account_receivables_on_admin_user_id"
+  add_index "account_receivables", ["company_id"], :name => "index_account_receivables_on_company_id"
+  add_index "account_receivables", ["customer_id"], :name => "index_account_receivables_on_customer_id"
 
   create_table "admin_dashboards", :force => true do |t|
     t.datetime "created_at", :null => false
