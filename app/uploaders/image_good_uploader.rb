e3e4f86fc8f5de01dee77a056bdb1ac96dda898b
Alexandre de Oliveira <chavedomundo@gmail.com>
2012-04-21 19:27:01 -0300

    [23896643] Replace RMagick with MiniMagick

--- app/uploaders/image_good_uploader.rb
+++ app/uploaders/image_good_uploader.rb
@@ -1,12 +1,7 @@
 # encoding: utf-8
 
 class ImageGoodUploader < CarrierWave::Uploader::Base
-
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  include CarrierWave::RMagick
+  include CarrierWave::MiniMagick
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
   # include Sprockets::Helpers::RailsHelper
   # include Sprockets::Helpers::IsolatedHelper
