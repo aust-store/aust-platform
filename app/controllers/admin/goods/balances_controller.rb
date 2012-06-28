267981af1bf25f5c13d946ec8ac71eef95f452e9
kurko <chavedomundo@gmail.com>
2012-06-28 00:50:59 -0300

    Added Unit tests for BalancesController

--- app/controllers/admin/goods/balances_controller.rb
+++ app/controllers/admin/goods/balances_controller.rb
@@ -1,43 +1,52 @@
-class Admin::Goods::BalancesController < Admin::ApplicationController
-  before_filter :load_good
-  before_filter :sanitize_params, only: [:create, :update]
+module Admin
+  module Goods
+    class BalancesController < Admin::ApplicationController
+      before_filter :sanitize_params, only: [:create, :update]
 
-  def index
-    @balances = Admin::GoodBalanceDecorator.decorate @good.balances
-  end
+      def index
+        # TODO load_good has no tests (e.g mock Good.where)
+        load_good
+        @balances = Admin::GoodBalanceDecorator.decorate(@good.balances)
+      end
 
-  def new
-    @balance = @good.balances.build
-  end
+      def new
+        load_good
+        @balance = @good.balances.build
+      end
 
-  def create
-    @balance = @good.balances.build params[:good_balance]
-    @balance.balance_type = "in"
-    if @balance.save
-      redirect_to admin_inventory_good_balances_url(@balance.good)
-    else
-      render "new"
+      def create
+        load_good
+        @balance = @good.balances.build params[:good_balance]
+        @balance.balance_type = "in"
+        if @balance.save
+          redirect_to admin_inventory_good_balances_url(@balance.good)
+        else
+          render "new"
+        end
+      end
+
+      def update
+        load_good
+        @balance = @good.balances.find params[:id]
+        if @balance.update_attributes params[:good_balance]
+          redirect_to admin_inventory_good_balances_url(@balance.good)
+        else
+          render "edit"
+        end
+      end
+
+      private
+
+      def load_good
+        @good = Good.where(id: params[:good_id]).within_company(current_user.company).first
+        raise "This doesn't belong to you" if @good.nil?
+      end
+
+      # TODO there's no test coverage for this
+      def sanitize_params
+        good_params = params[:good_balance]
+        params[:good_balance][:cost_per_unit] = Store::Currency.to_float good_params[:cost_per_unit]
+      end
     end
-  end
-
-  def update
-    @balance = @good.balances.find params[:id]
-    if @balance.update_attributes params[:good_balance]
-      redirect_to admin_inventory_good_balances_url(@balance.good)
-    else
-      render "edit"
-    end
-  end
-
-  private
-
-  def load_good
-    @good = Good.where(id: params[:good_id]).within_company(current_user.company).first
-    raise "This doesn't belong to you" if @good.nil?
-  end
-
-  def sanitize_params
-    good_params = params[:good_balance]
-    params[:good_balance][:cost_per_unit] = Store::Currency.to_float good_params[:cost_per_unit]
   end
 end
---
+++ spec/unit/controllers/admin/goods/balances_controller_spec.rb
@@ -0,0 +1,98 @@
+module Store; class Currency; end; end
+require "unit_spec_helper"
+require "controllers/admin/goods/balances_controller"
+
+describe Admin::Goods::BalancesController do
+
+  it_obeys_the "application controller contract"
+  it_obeys_the "Good model contract"
+
+  let(:valid_attributes) do
+    { "description"   => "These came from Japan.",
+      "quantity"      => "4",
+      "cost_per_unit" => "R$ 20.0" }
+  end
+  let(:sanitized_attributes) do
+    { "description"   => "These came from Japan.",
+      "quantity"      => "4",
+      "cost_per_unit" => 20.0 }
+  end
+
+  before do
+    @good = double
+    @balances = double
+    @balances.stub(:good).and_return(@good)
+    @balances.stub(:balance_type=)
+    subject.stub_chain(:current_user, :company) { 1 }
+
+    Store::Currency.stub(:to_float).with("R$ 20.0").and_return(20.0)
+
+    Good.stub_chain(:where, :within_company, :first).and_return(@good)
+  end
+
+  it "should raise if other company's id was given" do
+    Good.stub_chain(:where, :within_company, :first).and_return(nil)
+    expect do
+      get :index, good_id: invalid_good.id 
+    end.to raise_error
+  end
+
+  describe "#index" do
+    it "loads good's balances" do
+      @good.stub(:balances) { double(balances: @balances) }
+      Admin::GoodBalanceDecorator.stub(:decorate) { @balances }
+      subject.index.should == @balances
+    end
+  end
+
+  describe "#new" do
+    it "builds a new balance" do
+      @good.stub_chain(:balances, :build).and_return(@balances)
+      subject.new.should == @balances
+    end
+  end
+
+  describe "#create" do
+    before do
+      @good.stub_chain(:balances, :build).with(sanitized_attributes) { @balances }
+      subject.stub(:params) do
+        { good_balance: sanitized_attributes }
+      end
+    end
+
+    it "redirects if saving balance successfully" do
+      @balances.stub(:save) { true }
+      subject.stub(:admin_inventory_good_balances_url).with(@good) { "good"}
+      subject.should_receive(:redirect_to).with("good")
+      subject.create
+    end
+
+    it "renderform if not saving balance successfully" do
+      @balances.stub(:save) { false }
+      subject.should_receive(:render).with("new")
+      subject.create
+    end
+  end
+
+  describe "#update" do
+    before do
+      @good.stub_chain(:balances, :find).with("1").and_return(@balances)
+      subject.stub(:params) do
+        { id: "1", good_balance: sanitized_attributes }
+      end
+    end
+
+    it "redirects if updating the balance successfully" do
+      @balances.stub(:update_attributes).with(sanitized_attributes) { true }
+      subject.stub(:admin_inventory_good_balances_url).with(@good) { "good"}
+      subject.should_receive(:redirect_to).with("good")
+      subject.update
+    end
+
+    it "renders form if not updating balance successfully" do
+      @balances.stub(:update_attributes).with(sanitized_attributes) { false }
+      subject.should_receive(:render).with("edit")
+      subject.update
+    end
+  end
+end
