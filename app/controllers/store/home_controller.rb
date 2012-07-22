4731e8bec214047a545682565f7421668a235115
Alexandre de Oliveira <chavedomundo@gmail.com>
2012-07-21 21:59:11 -0300

    Fix failing specs regarding the list of goods in the main page

--- app/controllers/store/home_controller.rb
+++ app/controllers/store/home_controller.rb
@@ -2,7 +2,6 @@ class Store::HomeController < Store::ApplicationController
   layout "store"
 
   def index
-    @company = Company.where(handle: params[:store_id]).first
     @goods = @company.list_goods
   end
 end
