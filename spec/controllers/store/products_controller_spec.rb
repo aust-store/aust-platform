90840bf576786c752abc07bf9d5b36e89b553ccb
Alexandre de Oliveira <chavedomundo@gmail.com>
2012-09-23 15:27:14 +0900

    All store controller specs passing

--- spec/controllers/store/products_controller_spec.rb
+++ spec/controllers/store/products_controller_spec.rb
@@ -1,41 +1,12 @@
-require 'unit_spec_helper'
-
-class Product; end
-class Store::ApplicationController; end
-
-require "controllers/store/products_controller"
+require 'spec_helper'
 
 describe Store::ProductsController do
-  subject { Store::ProductsController.new }
-
-  describe "#show" do
+  describe "GET show" do
     it "loads a products by id" do
-      subject.stub(:params) { {id: 1} }
-      Good.stub(:find).with(1) { :product }
-      subject.show.should == :product
-    end
-  end
-
-  describe "#add_to_cart" do
-    let(:cart) { double }
-
-    before do
-      subject.stub(:params) { {id: 1} }
-      subject.stub(:cart) { cart }
-      subject.stub(:store_cart_path) { :cart_path }
-      subject.stub(:redirect_to)
-    end
-
-    it "adds the product into the cart" do
-      Good.stub(:find).with(1) { :product }
-      cart.should_receive(:add_item).with(:product)
-      subject.add_to_cart
-    end
-
-    it "redirects to the cart page" do
-      cart.stub(:add_item)
-      subject.should_receive(:redirect_to).with(:cart_path)
-      subject.add_to_cart
+      entries = double(inventory_entry: :entry)
+      Store::ItemsForSale.stub(:new).with(controller) { entries }
+      get :show, store_id: "name", id: 1
+      assigns(:product).should == :entry
     end
   end
 end
