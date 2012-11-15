ba728651016e391794eddb71dc32f9fb45306b1d
frontino <frontinojr@gmail.com>
2012-11-15 17:07:47 -0200

    [38937477] Auto fill bug fix edited

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,6 @@
 class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  # :token_authenticatable, :encryptable, :confirmable, :validatable, :lockable, :timeoutable and :omniauthable
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
@@ -8,14 +8,13 @@ class AdminUser < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me,
                   :company_attributes, :role, :name, :company_id
 
-  validates_presence_of :name
-  validates_uniqueness_of :name
-  validates_uniqueness_of :email, :case_sensitive => false, :allow_blank => true, :if => :email_changed?
-  validates_format_of :email, :with => Devise.email_regexp, :allow_blank => true, :if => :email_changed?
-  validates_presence_of :email, :on=>:create
-  validates_presence_of :password, :on=>:create
-  validates_confirmation_of :password, :on=>:create
-  validates_length_of :password, :within => Devise.password_length, :allow_blank => true
+  validates_presence_of :name, :email
+  validates_uniqueness_of :name, :scope => :company_id
+  validates_uniqueness_of :email, case_sensitive: false
+  validates_format_of :email, with: Devise.email_regexp
+  validates_presence_of :password, on: :create
+  validates_confirmation_of :password, on: :create
+  validates_length_of :password, within: Devise.password_length, allow_blank: true
 
 
   belongs_to :company
