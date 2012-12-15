41a22a0ca05438f015fa7e4dd9c3a9a8e1f6265d
Alexandre de Oliveira <chavedomundo@gmail.com>
2012-12-15 02:34:10 -0200

    Adds latest handlebars version

--- app/assets/javascripts/store/application_manifest.js
+++ app/assets/javascripts/store/application_manifest.js
@@ -1,7 +1,6 @@
 //= require_self
 //= require jquery
 //= require jquery_ujs
-//= require ember
 //= require emerald
 //= require_tree ./../../../../vendor/assets/javascripts/vendor/store/
 //= require_tree ./router/
