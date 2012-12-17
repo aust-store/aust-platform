16c9b81c50a16f082597ab12c65f43f0cde2a6c8
Alexandre de Oliveira <chavedomundo@gmail.com>
2012-12-16 23:41:56 -0200

    Fix admin login page not loading the current company

--- app/controllers/admin/devise/sessions_controller.rb
+++ app/controllers/admin/devise/sessions_controller.rb
@@ -1,23 +1,6 @@
 class Admin::Devise::SessionsController < Devise::SessionsController
   layout "admin/sign_in"
 
-  before_filter :load_store_information
-
-  private
-
-  def current_subdomain
-    if request.subdomain.present?
-      Array(request.subdomain).last
-    end
-  end
-
-  def load_store_information
-    if current_subdomain.present?
-      Rails.logger.info "Visiting #{current_subdomain} store."
-      @company ||= Company.find_by_handle(current_subdomain)
-    end
-  end
-
   def after_sign_in_path_for(resource)
     admin_dashboard_url
   end
