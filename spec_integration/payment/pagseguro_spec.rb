abe5e217450ed96d007c15be666552fd8bc4bf83
Alexandre de Oliveira <chavedomundo@gmail.com>
2012-12-29 18:01:45 -0200

    [41666649] Using the configured PagSeguro token during checkout

--- spec_integration/payment/pagseguro_spec.rb
+++ spec_integration/payment/pagseguro_spec.rb
@@ -9,7 +9,10 @@ describe Store::Payment::Pagseguro::Checkout do
   let(:shipping_options) { double(service_type: 'pac') }
   let(:user)             { FactoryGirl.create(:user) }
   let(:items)            { [double(id: 1, name: "T-Shirt", price: 12.0, quantity: 2)] }
-  let(:controller)       { double }
+
+  let(:payment_gateway)  { double(email: "chavedomundo@gmail.com", token: "7270EC0CE70F4FE8A1C6C4346CAC4417") }
+  let(:store)            { double(payment_gateway: payment_gateway) }
+  let(:controller)       { double(current_store: store) }
 
   let(:order)            { double(id:               2,
                                   shipping_address: shipping_address,
@@ -27,7 +30,7 @@ describe Store::Payment::Pagseguro::Checkout do
       payment.create_transaction
       url = payment.payment_url
       url.should include 'https://pagseguro.uol.com.br/v2/checkout/payment.html?code='
-      url.should =~ /\?code=.{32}/ 
+      url.should =~ /\?code=.{32}/
     end
   end
 end
