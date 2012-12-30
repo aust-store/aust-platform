164f2458c503f225f19dbf7441347936ea4432ea
Alexandre de Oliveira <chavedomundo@gmail.com>
2012-12-29 22:06:40 -0200

    [41666649] PagSeguro notifications working according to the configured token

--- spec/controllers/store/gateway_notifications/pagseguro_controller_spec.rb
+++ spec/controllers/store/gateway_notifications/pagseguro_controller_spec.rb
@@ -1,15 +1,28 @@
 require 'spec_helper'
 
 describe Store::GatewayNotifications::PagseguroController do
+  before do
+    @store = FactoryGirl.create(:company)
+    @store.build_payment_gateway(
+      email: "user@example.com",
+      name:  "pagseguro",
+      token: "1234"
+    )
+    @store.save
+    stub_subdomain(@store)
+  end
+
   describe "POST create" do
     it "triggers the status change" do
+      PagSeguro::Notification
+        .stub(:new)
+        .with("user@example.com", "1234", "notification_code")
+        .and_return(:notification)
 
-      PagSeguro::Notification.stub(:new).with("email", "token", "1234") { :notification }
       Store::GatewayNotifications::PagseguroWrapper.stub(:new).with(:notification) { :notification_wrapper }
-
       Store::Order::StatusChange.should_receive(:change).with(:notification_wrapper)
 
-      post :create, notificationCode: "1234"
+      post :create, notificationCode: "notification_code"
     end
   end
 end
