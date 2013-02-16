a8f7f78a1dc733ba61ec58528bba104ac2d922bd
Alexandre de Oliveira <chavedomundo@gmail.com>
2013-02-16 16:34:05 -0200

    [42444485] Strips whitespace from PagSeguro token

--- app/models/payment_gateway.rb
+++ app/models/payment_gateway.rb
@@ -3,6 +3,13 @@ class PaymentGateway < ActiveRecord::Base
 
   attr_accessible :email, :name, :store_id, :token
 
+  before_validation :sanitize_token
+
+  def sanitize_token
+    self.token = self.token.strip if self.token.present?
+    true
+  end
+
   def active?
     email.present? && name.present? && token.present?
   end
--- spec/models/payment_gateway_spec.rb
+++ spec/models/payment_gateway_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe PaymentGateway do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "callbacks" do
+    describe "sanitize_token on validation" do
+      it "strip token whitespaces on validation" do
+        gateway = PaymentGateway.new(token: " 1234 ")
+        expect(gateway.token).to eq " 1234 "
+        gateway.valid?
+        expect(gateway.token).to eq "1234"
+      end
+
+      it "doesn't try stripping token whitespaces if it's nil" do
+        gateway = PaymentGateway.new
+        expect(gateway.token).to be_nil
+        gateway.valid?
+        expect(gateway.token).to be_nil
+      end
+    end
+  end
 end
