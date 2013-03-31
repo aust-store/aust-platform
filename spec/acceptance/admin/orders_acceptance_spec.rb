76fb0ec101f6bd60ebd105c6afbf3a1bb5b6b86d
Alexandre de Oliveira <chavedomundo@gmail.com>
2013-03-31 20:59:25 -0300

    Fixes timezone

--- spec/acceptance/admin/orders_acceptance_spec.rb
+++ spec/acceptance/admin/orders_acceptance_spec.rb
@@ -10,7 +10,7 @@ feature "Orders Management" do
 
   describe "the index page" do
     before do
-      Timecop.travel(Time.local(2013, 03, 30, 10, 10, 10)) do
+      Timecop.travel(Time.utc(2013, 03, 30, 10, 10, 10)) do
         @pending_order = FactoryGirl.create(:order, store: @company, total_items: 3)
         @paid_order    = FactoryGirl.create(:paid_order, store: @company, total_items: 5)
       end
@@ -24,7 +24,7 @@ feature "Orders Management" do
         within(".payment_status")  { page.should have_content "Aguardando pgto." }
         within(".items_quantity")  { page.should have_content "3 itens" }
         within(".total")           { page.should have_content "R$ 41,82" }
-        within(".purchase_date")   { page.should have_content "30/03/2013 13:10" }
+        within(".purchase_date")   { page.should have_content "30/03/2013 07:10" }
       end
 
       within "#order_card_#{@paid_order.id}" do
@@ -32,7 +32,7 @@ feature "Orders Management" do
         within(".payment_status")  { page.should have_content "Pagamento Completo" }
         within(".items_quantity")  { page.should have_content "5 itens" }
         within(".total")           { page.should have_content "R$ 69,70" }
-        within(".purchase_date")   { page.should have_content "30/03/2013 13:10" }
+        within(".purchase_date")   { page.should have_content "30/03/2013 07:10" }
       end
     end
   end
