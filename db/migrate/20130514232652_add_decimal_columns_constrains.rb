ee120471d12211c757ba4f13d980d46b7c4e85d7
Sebastian Sogamoso <sebasoga@gmail.com>
2013-05-15 20:58:37 -0500

    [NO-STORY] Fix `down` method on migration

--- db/migrate/20130514232652_add_decimal_columns_constrains.rb
+++ db/migrate/20130514232652_add_decimal_columns_constrains.rb
@@ -14,8 +14,8 @@ class AddDecimalColumnsConstrains < ActiveRecord::Migration
     change_column :account_receivables, :value, :decimal, null: true, default: nil
     change_column :inventory_entries, :quantity, :decimal, null: true, default: nil
     change_column :inventory_entries, :cost_per_unit, :decimal, null: true, default: nil
-    change_column :inventory_item_prices, :value, :decimal, null: true, default: nil
-    change_column :inventory_items, :moving_average_cost, :decimal, null: true, default: nil
+    change_column :inventory_item_prices, :value, :decimal, null: true, default: nil, precision: 8, scale: 2
+    change_column :inventory_items, :moving_average_cost, :decimal, null: true, default: nil, precision: 8, scale: 2
     change_column :order_items, :price, :decimal, null: true, default: nil
     change_column :order_items, :quantity, :decimal, null: true, default: nil
     change_column :order_shippings, :price, :decimal, null: true, default: nil
