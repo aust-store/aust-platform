83301971951050cfc3af803e79fb112574a51083
Arthur de Oliveira <arthur1606@gmail.com>
2013-05-31 17:17:42 -0300

    [38938429] Update Shoulda_matchers

--- spec/models/page_spec.rb
+++ spec/models/page_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe Page do
   describe "validations" do
-    it { should validate_presence_of (:company) }
-    it { should validate_presence_of (:title) }
-    it { should validate_presence_of (:body) }
+    it { should validate_presence_of :company }
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :body }
   end
 end
