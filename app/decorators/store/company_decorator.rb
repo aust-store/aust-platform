1b1d664a63ff99c2605c6f0659c1439cae25f452
Alexandre de Oliveira <chavedomundo@gmail.com>
2013-07-01 10:36:34 -0300

    [25500151] Adds footer with company contact information

--- app/decorators/store/company_decorator.rb
+++ app/decorators/store/company_decorator.rb
@@ -6,5 +6,14 @@ module Store
     def complete_address
       address.complete_address if model.address.present?
     end
+
+    def complete_contact
+      phones = []
+      if model.contact.present?
+        phones << model.contact.phone_1
+        phones << model.contact.phone_2
+      end
+      phones.compact.join(", ")
+    end
   end
 end
