d777cc2b6050e2b994e65d6f4b7252fd638e9aba
Alexandre de Oliveira <chavedomundo@gmail.com>
2013-11-21 20:07:14 -0200

    [60941398] Adds delay before saving the textarea to server

--- app/assets/javascripts/theme_editor/controllers/theme_file_controller.js
+++ app/assets/javascripts/theme_editor/controllers/theme_file_controller.js
@@ -1,7 +1,14 @@
 App.ThemeFileController = Ember.ObjectController.extend({
+  saveTimer: null,
+
   saveBody: function() {
     if (this.get('isDirty')) {
-      this.get('model').save();
+      var _this = this;
+
+      clearTimeout(this.saveTimer);
+      this.saveTimer = setTimeout(function() {
+        _this.get('model').save();
+      }, 4000);
     }
   }.observes('body'),
 });
