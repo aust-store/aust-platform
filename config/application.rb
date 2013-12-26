bf35b8e0947e296c1aef26a762311d261a99a58f
Alexandre de Oliveira <chavedomundo@gmail.com>
2013-12-26 04:33:16 -0200

    Fixes Rails warning

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ Bundler.require(:default, Rails.env)
 
 module Store
   class Application < Rails::Application
+    config.i18n.enforce_available_locales = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
